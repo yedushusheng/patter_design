2cac19cf4e5032adc8d0c5d57839bb70c432b31f
yedushusheng <yedushusheng@163.com>
2018-11-16 21:54:27 +0800

    工厂方法模式code

--- Factory.java
+++ Factory.java
@@ -1,3 +1,4 @@
+//简单工厂模式
 public class Operation{
 	private double _number1 = 0;
 	private double _number2 = 0;
@@ -60,3 +61,22 @@ oper = OperationFactory.createOperation("+");
 oper.number1 = 1;
 oper.number2 = 2;
 double result = oper.GetResult()
+
+
+//工厂方法模式
+interface IFactory{
+	Operation CreateOperation();
+}
+
+class AddFactpry:IFactory{
+	public Operation CreateOperation(){
+		return new OperationAdd();
+	}
+}
+
+//客户端代码：
+IFactory operFactory = new AddFactpry();
+Operation oper = operFactory.CreateOperation();
+oper.number1 = 1;
+oper.number2 = 2;
+double result = oper.GetResult();
