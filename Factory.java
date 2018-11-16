60d287f33ef0d0e2e481bdfee73b7aa354f7a9ea
yedushusheng <yedushusheng@163.com>
2018-11-16 21:39:46 +0800

    工厂模式java代码实现

---
+++ Factory.java
@@ -0,0 +1,62 @@
+public class Operation{
+	private double _number1 = 0;
+	private double _number2 = 0;
+
+	public double number1(){
+		_number1 = value;
+	}
+
+	public double number2(){
+		_number2 = value;
+	}
+
+	public virtual double GetResult(){
+		double result = 0;
+		return result;
+	}
+}
+
+class OperationAdd:Operation{
+	public Override double GetResult(){
+		double result = 0;
+		result = number1 + number2;
+		return result;
+	}
+}
+
+class OperationSub:Operation{
+	public Override double GetResult(){
+		double result = 0;
+		result number1 - number2;
+		return result;
+	}
+}
+
+public class OperationFactory{
+	public static Operation createOperation(string operation){
+		Operation oper = null;
+		switch(operation){
+			case “+”：
+				oper = new OperationAdd();
+				break;
+			case "=":
+				oper = new OperationSub();
+				break;
+			case "*":
+				oper = new OperationMul();
+				break;
+			case "/":
+				oper = new OperationDiv();
+				break;
+		}
+	return oper;
+	}
+}
+
+
+//客户端代码：
+Operation oper;
+oper = OperationFactory.createOperation("+");
+oper.number1 = 1;
+oper.number2 = 2;
+double result = oper.GetResult()
