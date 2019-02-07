92578d29996a704c8f3553095c69c1108352d0ed
yedushusheng <yedushusheng@163.com>
2019-02-07 22:17:46 +0800

    Create Singleton.java

---
+++ 1、创建型模式/单例模式/Singleton.java
@@ -0,0 +1,19 @@
+class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+
+    }
+
+    public static  Singleton GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
